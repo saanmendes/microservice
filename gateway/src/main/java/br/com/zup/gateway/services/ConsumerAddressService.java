366b1a3a60439e4b8a661f45f2627f7a92b0834b
Sandra Mendes <sandra.mendes@gmail.com>
2025-01-03 13:23:01 -0300

    Add crud on service

--- gateway/src/main/java/br/com/zup/gateway/services/ConsumerAddressService.java
+++ gateway/src/main/java/br/com/zup/gateway/services/ConsumerAddressService.java
@@ -10,6 +10,11 @@ import br.com.zup.gateway.infra.clients.consumer.dtos.ConsumerRegisterDTO;
 import br.com.zup.gateway.infra.clients.consumer.dtos.ConsumerResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class ConsumerAddressService {
@@ -35,6 +40,33 @@ public class ConsumerAddressService {
         AddressRegisterDto addressRegisterDto = mapToAddressRegisterDTO(consumerAddressRegisterDTO, consumerId);
         return addressClient.registeAddress(addressRegisterDto);
     }
+
+    public ConsumerResponseDTO getConsumer(String consumerId) {
+        return consumerClient.getConsumer(consumerId);
+    }
+
+    public AddressResponseDTO getAddress(String addressId) {
+        return addressClient.getAddress(addressId);
+    }
+
+    public ConsumerResponseDTO updateConsumer(String consumerId, ConsumerAddressRegisterDTO consumerAddressRegisterDTO) {
+        ConsumerRegisterDTO consumerRegisterDTO = mapToConsumerRegisterDTO(consumerAddressRegisterDTO);
+        return consumerClient.updateConsumer(consumerId, consumerRegisterDTO);
+    }
+
+    public AddressResponseDTO updateAddress(String addressId, ConsumerAddressRegisterDTO consumerAddressRegisterDTO) {
+        AddressRegisterDto addressRegisterDto = mapToAddressRegisterDTO(consumerAddressRegisterDTO, addressId);
+        return addressClient.updateAddress(addressId, addressRegisterDto);
+    }
+
+    public void deleteConsumer(String consumerId) {
+        consumerClient.deleteConsumer(consumerId);
+    }
+
+    public void deleteAddress(String addressId) {
+        addressClient.deleteAddress(addressId);
+    }
+
 
     private ConsumerRegisterDTO mapToConsumerRegisterDTO(ConsumerAddressRegisterDTO consumerAddressRegisterDTO) {
         ConsumerRegisterDTO consumerRegisterDTO = new ConsumerRegisterDTO();
